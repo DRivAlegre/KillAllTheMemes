ed0fcdd92c12d62d0b8c37e119f1d39b86c0e06d
DRivAlegre <david.davidsociety.rivero@gmail.com>
2015-06-09 16:00:27 -0300

    Enable social buttons

--- js/scroll.js
+++ js/scroll.js
@@ -5,9 +5,6 @@ window.addEventListener("scroll",function(){
 	var scroll = scroller();
 	var scroll_size = scrollSize();
 
-/*	console.log("Scroll is: " + scroll);
-	console.log("Scroll_size is: " + (scroll_size - 500));*/
-
     if(scroll > (scroll_size - 500)){
 		var imgNs = JSON.parse("[" + localStorage.getItem("ls.imgs") + "]")[0];
 		var sectionmain = document.getElementsByClassName("content")[0];
@@ -17,10 +14,11 @@ window.addEventListener("scroll",function(){
 		   	var img_img = document.createElement("img");
 		   	img_img.src = imgNs[j].url;
 		   	node.appendChild(img_img);
+		   	getSocialButtons(node);
 		   	sectionmain.appendChild(node);
 		}
-		initial += 50;
-		finale += 50;
+		initial += 70;
+		finale += 70;
     } 
 });
 
@@ -32,4 +30,17 @@ var scrollSize = function(){
 	    var element = document.getElementsByTagName("body")[0];
 	    var scroll_size = element.offsetHeight || element.style.pixelHeight;
 	    return scroll_size;
+}
+
+
+var getSocialButtons(elem){
+	var social = document.createElement("div");
+	social.className = "social";
+	social.innerHTML = "<div class='facebook'>
+						<div id='fb-root'></div>
+						<div class='fb-like' data-href='https://drivalegre.github.io/KillAllTheMemes' data-layout='button' data-action='like' data-show-faces='false' data-share='true'></div>
+					<div class='twitter'>
+						<a href='https://twitter.com/share' class='twitter-share-button' data-url='http://drivalegre.github.io/KillAllTheMemes' data-text='http://fyslab.net/killallthememes' data-related='killallthememes' data-count='none' data-hashtags='KillAllTheMemes'>Tweet</a>
+					</div>";
+	elem.appendChild(social);
 }

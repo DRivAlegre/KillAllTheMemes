6cbff27d6e76f991541ccf429819dc1bcc236993
DRivAlegre <david.davidsociety.rivero@gmail.com>
2015-06-08 23:01:01 -0300

    Lazy load imgs

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,21 @@
 window.addEventListener("scroll",function(){
-	var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-    // Obtenemos el alto del header
-    var element = document.getElementByTagName("body");
-    var scroll_size = element.offsetHeight || element.style.pixelHeight;
+	
+
+    if(scroll > scroll_size){
+        // Codigo para mostrar menu
+    } else {
+        // Codigo para ocultar el menu
+    }
 });
+
+var getData = {
+	"scroll":function(){
+		var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+		return scrollT;
+	},
+	"scrollSize":function(){
+	    var element = document.getElementByTagName("body");
+	    var scroll_size = element.offsetHeight || element.style.pixelHeight;
+	    return scroll_size;
+	}
+}

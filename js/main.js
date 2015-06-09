c727057e84418ec54e7ad533f6352b5e30af8650
DRivAlegre <david.davidsociety.rivero@gmail.com>
2015-06-08 22:08:35 -0300

    Changes js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-var m = angular.module("katmMod",["localStorageModule"])
-.controller("katmC",function($scope,$http,localStorageService){
+angular.module("katmMod",["localStorageModule"])
+.controller("katmC",function(localStorageService,$http,$scope){
 	$scope.imgs = [];
 	$scope.actualImgs = [];
 	$scope.initial = 0;
 	$scope.finale = 35;
 
+	// A comment
 	if(localStorageService.get("imgs")){
 		$scope.imgs = localStorageService.get("imgs");
 	}
 	else{
-		$http.get("http://drivalegre.github.io/KillAllTheMemes/js/jsondata.json")
+		$http.get("http://localhost:8000/js/jsondata.json")
 		.success(function(data){
 			$scope.imgs = data;
 			localStorageService.set("imgs",$scope.imgs);

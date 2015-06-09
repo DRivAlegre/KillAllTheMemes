b24fff6b3e3ba7171eb02cc9e782a8d1dd91da92
DRivAlegre <david.davidsociety.rivero@gmail.com>
2015-06-09 14:47:47 -0300

    Running scroll.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ angular.module("katmMod",["LocalStorageModule"])
 .controller("katmC",function(localStorageService,$http,$scope){
 	$scope.imgs = [];
 	$scope.actualImgs = [];
-	$scope.initial = 0;
-	$scope.finale = 29;
 
 	// A comment
 	if(localStorageService.get("imgs")){
@@ -21,38 +19,5 @@ angular.module("katmMod",["LocalStorageModule"])
 	}
 
 	var ims = $scope.imgs;
-	$scope.actualImgs = ims.slice($scope.initial,$scope.finale);
-	$scope.initial += 30;
-	$scope.finale += 30;
-
-	$scope.getMorePages = function(){
-		var ims = $scope.imgs;
-		$scope.actualImgs = ims.slice($scope.initial,$scope.finale);
-		$scope.initial += 30;
-		$scope.finale += 30;
-	}
-
-	//
-
-	/*window.addEventListener("scroll",function(){
-	var scroll = getData.scroll();
-	var scroll_size = getData.scrollSize();
-
-	    if(scroll > scroll_size){
-	        // Linkea el evento scroll con $scope.getMorePages() y dibujalo en la pagina con append
-	    	$scope.getMorePages();
-	    } 
-	});
-
-	var getData = {
-		"scroll":function(){
-			var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
-			return scrollT;
-		},
-	"scrollSize":function(){
-	    var element = document.getElementByTagName("body");
-	    var scroll_size = element.offsetHeight || element.style.pixelHeight;
-	    return scroll_size;
-		}
-	}*/
+	$scope.actualImgs = ims.slice(0,29);
 });
--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,35 @@
-window.addEventListener("scroll",function(){
-	var scroll = getData.scroll();
-	var scroll_size = getData.scrollSize();
+var initial = 30;
+var finale = 59;
 
-    if(scroll > scroll_size){
-        // Linkea el evento scroll con $scope.getMorePages() y dibujalo en la pagina con append
-    	$scope.getMorePages();
+window.addEventListener("scroll",function(){
+	var scroll = scroller();
+	var scroll_size = scrollSize();
+
+/*	console.log("Scroll is: " + scroll);
+	console.log("Scroll_size is: " + (scroll_size - 500));*/
+
+    if(scroll > (scroll_size - 500)){
+		var imgNs = JSON.parse("[" + localStorage.getItem("ls.imgs") + "]")[0];
+		var sectionmain = document.getElementsByClassName("content")[0];
+		for(var j = initial;j < finale;j++){
+		   	var node = document.createElement("figure");
+		   	node.className = "pict ng-scope";
+		   	var img_img = document.createElement("img");
+		   	img_img.src = imgNs[j].url;
+		   	node.appendChild(img_img);
+		   	sectionmain.appendChild(node);
+		}
+		initial += 50;
+		finale += 50;
     } 
 });
 
-var getData = {
-	"scroll":function(){
+var scroller = function(){
 		var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
 		return scrollT;
-	},
-	"scrollSize":function(){
-	    var element = document.getElementByTagName("body");
+}
+var scrollSize = function(){
+	    var element = document.getElementsByTagName("body")[0];
 	    var scroll_size = element.offsetHeight || element.style.pixelHeight;
 	    return scroll_size;
-	}
 }

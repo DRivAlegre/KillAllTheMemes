7dea912efdc76a6631cf00ae52761407da1b023f
guest-user <guest-user@192.168.1.7>
2017-03-19 13:53:22 -0300

    Remove scroll script

--- js/main.js
+++ js/main.js
@@ -1,23 +1,58 @@
 angular.module("katmMod",["LocalStorageModule"])
-.controller("katmC",function(localStorageService,$http,$scope){
+.controller("katmC",function(localStorageService, $http, $scope, $window){
 	$scope.imgs = [];
 	$scope.actualImgs = [];
 
+	var AMOUNT_IMAGES = 29;
+	var pageImages = 1;
+	var currentScroll = scroller();
+	var maxScroll = scrollSize();
+
 	// A comment
-	if(localStorageService.get("imgs")){
+	if (localStorageService.get("imgs")) {
 		$scope.imgs = localStorageService.get("imgs");
 	}
-	else{
+	else {
 		$http.get("http://david-rivero.github.io/KillAllTheMemes/js/jsondata.json")
-		.success(function(data){
+		.success(function (data) {
 			$scope.imgs = data;
-			localStorageService.set("imgs",$scope.imgs);
+			localStorageService.set("imgs", $scope.imgs);
 		})
-		.error(function(err){
+		.error(function (err) {
 			console.log(err);
 		});
 	}
+	updateImages();
 
-	var ims = $scope.imgs;
-	$scope.actualImgs = ims.slice(0,29);
+	$window.addEventListener('scroll', function () {
+		currentScroll = scroller();
+		if (currentScroll >= maxScroll) {
+			pageImages++;
+			updateImages();
+
+			if (currentScroll > maxScroll) {
+				maxScroll = scrollSize();
+			}
+		}
+	});
+
+	function updateImages () {
+		var ims = $scope.imgs;
+		$scope.actualImgs = ims.slice(0, pageImages * AMOUNT_IMAGES);
+		// FIXME: Update in 'angular' way
+		if (pageImages > 1) {
+			$scope.$apply();
+		}
+	}
+
+	function scroller () {
+		var scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+		return scrollT;
+	}
+
+	function scrollSize () {
+	    var element = document.getElementsByTagName("body")[0];
+	    var scroll_size = element.offsetHeight || element.style.pixelHeight;
+	    return scroll_size;
+	}
 });
